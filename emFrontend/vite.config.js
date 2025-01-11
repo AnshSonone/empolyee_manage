6fd7665ac91e21e2e25363614b2385ca6e621105
AnshSonone <118267956+AnshSonone@users.noreply.github.com>
2025-01-11 19:53:47 +0530

    Update vite.config.js

--- emFrontend/vite.config.js
+++ emFrontend/vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  const env = loadEnv(mode, process.cwd())
+  
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'import.meta.env.VITE_API_URL',
+        target: env.VITE_API_URL,
         changeOrigin: true,
         secure: true,
       }
