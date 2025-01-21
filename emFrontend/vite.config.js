bef0120b0463ae32a5e6631be116caf74a1cf9cc
AnshSonone <118267956+AnshSonone@users.noreply.github.com>
2025-01-21 19:30:42 +0530

    Update vite.config.js

--- emFrontend/vite.config.js
+++ emFrontend/vite.config.js
@@ -1,35 +1,17 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-export default ({mode}) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: "http:///localhost:8000",
-          changeOrigin: true,
-          secure: true,
-        }
+// https://vite.dev/config/
+export default defineConfig({
+  
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: "https://ansh0r.pythonanywhere.com/",
+        changeOrigin: true,
+        secure: true,
       }
     }
-  })
-}
-
-// https://vite.dev/config/
-// export default defineConfig({
-  
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: import.meta.env.VITE_API_URL,
-//         changeOrigin: true,
-//         secure: true,
-//       }
-//     }
-//   }
-// })
+  }
+})
