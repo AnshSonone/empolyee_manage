f73bbcca961132cfc927f326645c2a91c6f860e2
AnshSonone <118267956+AnshSonone@users.noreply.github.com>
2025-01-21 18:53:27 +0530

    Update vite.config.js

--- emFrontend/vite.config.js
+++ emFrontend/vite.config.js
@@ -1,45 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load environment variables
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL, // Use the environment variable for the proxy target
-          changeOrigin: true,
-          secure: true,
-        },
-      },
-    },
-  };
-});
-
-
-
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-
-//   const env = loadEnv(mode, process.cwd())
+export default defineConfig({
   
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: env.VITE_API_URL,
-//         changeOrigin: true,
-//         secure: true,
-//       }
-//     }
-//   }
-// })
-
-// import.meta.env.VITE_API_URL ||
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: import.meta.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
+})
